7493b022d5e56bdc0538a26dd36954480cc0b74f
XueqianDeng <denghokin@gmail.com>
2024-04-08 01:52:05 -0400

    r is not very useful

--- R_analysis/plot.R
+++ R_analysis/plot.R
@@ -2,4 +2,7 @@ library(ggplot2) # for making plots
 
 library(magrittr) # for "pipe"-like coding in R
 
-readcounts <- read.table("featCounts_Gierlinski_genes.txt", header=TRUE)
+SRR14624456 <- read.table("/Users/billdeng/PycharmProjects/happy_rna/R_analysis/SRR14624456.txt", header=TRUE)
+str(SRR14624456)
+BiocManager::install("DESeq2") # only needs to be done once!
+library(DESeq2)
